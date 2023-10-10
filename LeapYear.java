85844cdf56ae64461e42d07beccca476f7990616
BuildTools <unconfigured@null.spigotmc.org>
2023-10-10 08:52:38 +0700

    leap Year

---
+++ LeapYear.java
@@ -0,0 +1,16 @@
+public class LeapYear {
+    public static void main(String[] args) {
+        System.out.println(isLeapYear(1600));
+    }
+
+    public static boolean isLeapYear(int year){
+        if (year < 1 || year > 9999){
+            System.out.println("False");
+            return false;
+        } else if ( (year %4 == 0 && year %100 != 0 ) || year %400 == 0){
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
