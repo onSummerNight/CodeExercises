3ae90f090a65e1481c7d294eb1606912683372fa
BuildTools <unconfigured@null.spigotmc.org>
2023-10-22 00:27:42 +0700

    add new challange

---
+++ InputCalculator.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class InputCalculator {
+    public static void main(String[] args) {
+        inputThenPrintSumAndAverage();
+    }
+
+    public static void inputThenPrintSumAndAverage(){
+        Scanner scanner = new Scanner(System.in);
+        int counter = 0;
+        int sum = 0;
+        long average = 0;
+        while (true){
+            try {
+                String inputNumber = scanner.nextLine();
+
+                sum = sum + Integer.parseInt(inputNumber);
+                counter++;
+            } catch (NumberFormatException e){
+                break;
+            }
+        }
+        average  = Math.round((double) sum / counter );
+        System.out.println("SUM = " + sum + " AVG = " + average);
+    }
+}
